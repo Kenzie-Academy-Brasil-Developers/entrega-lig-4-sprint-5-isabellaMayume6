1e750dfc7b363cfb6e50c5a0effd47ae6d591cd6
yasmin <yasminmartinsdebrito@gmail.com>
2021-10-04 14:54:59 -0300

    discos prontos

---
+++ assets/script.js
@@ -0,0 +1,7 @@
+const player1 = document.createElement('div');
+player1.classList('player1')
+document.body.appendChild(player1)
+
+const player2 = document.createElement('div');
+player2.classList('player2')
+document.body.appendChild(player2)
