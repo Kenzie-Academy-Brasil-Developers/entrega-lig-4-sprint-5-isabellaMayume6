9defe86bc4735d6befe23bf910df2f6e97d9e40b
Lucas Anacleto <lucasanacleto@outlook.com.br>
2021-10-04 16:01:43 -0300

    mapa feito

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+let mainGame = document.getElementById('jogo')
+let jogo = document.createElement('div')
+jogo.setAttribute('class', 'jogoClass')
+mainGame.appendChild(jogo)
+
+function gerandoMapa() {
+    for (let i = 1; i <= 7; i++) {
+        let coluna = document.createElement('div')
+        coluna.classList.add('coluna', 'coluna' + i)
+        jogo.appendChild(coluna)
+
+        for (let j = 1; j <= 6; j++) {
+            let linha = document.createElement('div')
+            linha.classList.add('linha', 'linha' + j)
+            coluna.appendChild(linha)
+        }
+    }
+}
+
+gerandoMapa()
