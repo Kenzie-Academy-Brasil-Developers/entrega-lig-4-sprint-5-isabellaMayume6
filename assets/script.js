164bbef3666cb6c9c74c6de783b31afec98f1d0b
MateusVSousa <min705555@gmail.com>
2021-10-04 18:41:49 -0300

    0.1 try(dikinput)

--- assets/script.js
+++ assets/script.js
@@ -1,31 +1,60 @@
+//Global Var
+let count = 0;
+
 // discos yasmin
 const player1 = document.createElement('div');
-player1.classList('player1')
-document.body.appendChild(player1)
+player1.classList = 'player1'
+// document.body.appendChild(player1)
 
 const player2 = document.createElement('div');
-player2.classList('player2')
-document.body.appendChild(player2)
+player2.classList = 'player2'
+// document.body.appendChild(player2)
 
 // ---------------- GERANDO MAPA ------------------//
 let mainGame = document.getElementById('jogo')
 let jogo = document.createElement('div')
 jogo.setAttribute('class', 'jogoClass')
 mainGame.appendChild(jogo)
-
+let coluna = document.createElement('div');
+let linha = document.createElement('div');
 function gerandoMapa() {
     for (let i = 1; i <= 7; i++) {
         let coluna = document.createElement('div')
-        coluna.classList.add('coluna', 'coluna' + i)
+        coluna.classList.add('coluna')
         jogo.appendChild(coluna)
-
+        
         for (let j = 1; j <= 6; j++) {
             let linha = document.createElement('div')
-            linha.classList.add('linha', 'linha' + j)
+            linha.classList.add('linha')
             coluna.appendChild(linha)
         }
     }
 }
 
 gerandoMapa()
+document.body.addEventListener("click",diskFall);
+
+
+function diskFall (Event) {
+    let alvo = Event.path[0]
+    let input = Event.path[1].lastChild
+    let actual = input.childElementCount
+    console.log(Event.path[1])
+    if(alvo.childElementCount === 0){
+        if (count%2 === 0 && alvo.className === "linha"){
+            const player1 = document.createElement('div');
+            player1.classList = 'player1'
+            alvo.appendChild(player1);
+            count++
+        }else if (count%2 === 1 && alvo.className === "linha") {
+            const player2 = document.createElement('div');
+            player2.classList = 'player2'
+            alvo.appendChild(player2);
+            count++
+        }
+    }else {
+        actual --
+    }
+    
+};
 
