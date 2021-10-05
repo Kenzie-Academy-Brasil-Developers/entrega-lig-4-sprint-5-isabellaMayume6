04adc63aa39c6a24ca84ec6755e204d382992907
Lucas Anacleto <lucasanacleto@outlook.com.br>
2021-10-05 18:40:38 -0300

    refatorando mapa e atribuindo ids nas colunas

--- assets/script.js
+++ assets/script.js
@@ -25,20 +25,22 @@ let arrayElementos = [
 
 let mainGame = document.getElementById('jogo')
 let jogo = document.createElement('div')
-jogo.setAttribute('class', 'jogoClass')
+jogo.setAttribute('id', 'jogoClass')
 mainGame.appendChild(jogo)
+
 let coluna = document.createElement('div');
 let linha = document.createElement('div');
 
 function gerandoMapa() {
     for (let i = 0; i < arrayElementos.length; i++) {
         let coluna = document.createElement('div')
-        coluna.classList.add('coluna', 'coluna' + i)
+        coluna.classList.add('coluna')
+        coluna.setAttribute('id', 'coluna' + i)
         jogo.appendChild(coluna)
         for (let j = 0; j < arrayElementos[i].length; j++) {
 
             let linha = document.createElement('div')
-            linha.classList.add('linha', 'linha' + j)
+            linha.classList.add('linha')
             coluna.appendChild(linha)
         }
     }
