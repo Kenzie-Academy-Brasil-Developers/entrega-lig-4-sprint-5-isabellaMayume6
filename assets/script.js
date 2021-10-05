0c859a3aab481da369609dc75a2d4b42fa336c2d
Lucas Anacleto <lucasanacleto@outlook.com.br>
2021-10-05 14:03:17 -0300

    adicionando array do mapa

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,39 @@
 // discos yasmin
 const player1 = document.createElement('div');
-player1.classList('player1')
+player1.classList.add('player1')
 document.body.appendChild(player1)
 
 const player2 = document.createElement('div');
-player2.classList('player2')
+player2.classList.add('player2')
 document.body.appendChild(player2)
 
 // ---------------- GERANDO MAPA ------------------//
+
+let arrayElementos = [
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
+]
+
+let arrayteste = []
+
 let mainGame = document.getElementById('jogo')
 let jogo = document.createElement('div')
 jogo.setAttribute('class', 'jogoClass')
 mainGame.appendChild(jogo)
 
 function gerandoMapa() {
-    for (let i = 1; i <= 7; i++) {
+    for (let i = 0; i < arrayElementos.length; i++) {
         let coluna = document.createElement('div')
         coluna.classList.add('coluna', 'coluna' + i)
         jogo.appendChild(coluna)
 
-        for (let j = 1; j <= 6; j++) {
+
+        for (let j = 0; j < arrayElementos[i].length; j++) {
             let linha = document.createElement('div')
             linha.classList.add('linha', 'linha' + j)
             coluna.appendChild(linha)
@@ -28,4 +42,3 @@ function gerandoMapa() {
 }
 
 gerandoMapa()
-
